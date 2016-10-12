37f29ef33a199c31dac76be657755b46ee0de6f9
Aleksey Bulatov <AlexeyABulatov@yandex.ru>
2016-10-12 17:30:58 +0300

    make examples added

--- bad-practice/script-building/build.sh
+++ bad-practice/script-building/build.sh
@@ -3,7 +3,9 @@
 mkdir -p build
 
 # Creating static library
-g++ -Wall -g -O0 -g -D_DEBUG -std=gnu++11 -o build/libsource.cpp.o -c lib/libsource.cpp
-ar qc build/libexample-lib.a  build/libsource.cpp.o 
-g++ -Wall -g -O0 -g -D_DEBUG -std=gnu++11 -o build/main.cpp.o -c program/main.cpp -I lib
-g++ -Wall -g -O0 -g -D_DEBUG  build/main.cpp.o -o build/program -lexample-lib -lboost_program_options -L build
+g++ -Wall -g -O0 -pg -DDEBUG -std=gnu++11 -o build/libsource.cpp.o -c lib/libsource.cpp
+ar qc build/libexample-lib.a  build/libsource.cpp.o
+# Compiling program
+g++ -Wall -g -O0 -pg -DDEBUG -std=gnu++11 -o build/main.cpp.o -c program/main.cpp -I lib
+# Linking
+g++ -Wall -g -O0 -pg -DDEBUG  build/main.cpp.o -o build/program -lexample-lib -lboost_program_options -L build
